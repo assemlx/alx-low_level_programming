23447181bba7a56e936e3427a3fe4661206e1070
Assem <asemstudy86@gmail.com>
2023-07-18 13:33:28 -0700

    add and print to 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *add - adds two integers
+  * @i: int type number
+  * @j: int type number
+  *Return: 0
+  */
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+	return (result);
+}
