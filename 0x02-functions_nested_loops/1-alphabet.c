a197316fee54fc0f7d78c659dfd8361740c816af
Assem <asemstudy86@gmail.com>
2023-07-18 06:47:57 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *print_alphabet - prints the alphabet in lowercase
+  *Return: 0
+  */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - print alphabet in lowercase 10 times
+  *Return: 0
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+
+}
