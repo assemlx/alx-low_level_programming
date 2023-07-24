d0615a26d02d6e2606d2aa0923b5d1abada963cd
Assem <asemstudy86@gmail.com>
2023-07-24 05:58:11 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *swap_int - swab two numbers
+  *@a: first number to swap
+  *@b: second nuber to swab
+  *Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	int temb;
+
+	*a = temb;
+	*a = *b;
+	*b = temb;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H_INCLUDED
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
