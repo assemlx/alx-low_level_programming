a9c1e0b42b07cac83983729f0fd3f883e49542f5
Assem Ali <asemstudy86@gmail.com>
2023-07-27 10:00:11 -0700

    mediuam push

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *string_toupper - transform the string letters to uppercase
+ * @str: the input string
+ * Return: the uppercase string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+	return (str);
+}
