0638cede103f7ba9b334127cfd8b30556be4ebad
Assem Ali <asemstudy86@gmail.com>
2023-08-07 16:37:00 -0700

    concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	l1 = 0;
 	while (s1[l1] != '\0')
 		l1++;
