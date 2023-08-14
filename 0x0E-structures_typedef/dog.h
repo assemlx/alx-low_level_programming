6b88a7e27b4e31f0cde1e10ef7af7240e8089405
Assem Ali <asemstudy86@gmail.com>
2023-08-14 13:41:12 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dogs`s name
+ * @age: dog`s age
+ * @owner: dog`s owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
