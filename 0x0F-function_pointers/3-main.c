bd0af1f3be7494f9b053418883733ab6cda9786d
Assem Ali <asemstudy86@gmail.com>
2023-08-16 09:20:37 -0700

    calculations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *operator;
+	int (*op_func)(int, int);
 
 	/* Error if arguments are not 4 with the executable */
 	if (argc != 4)
@@ -29,23 +29,22 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]); /*if argument is string convert to number*/
 	num2 = atoi(argv[3]); /* as stated above */
-	operator = argv[2];
 
+	op_func = get_op_func(argv[2]);
 	/* Error if operator is not null or a 2d array '\*' */
-	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	if (!op_func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
 	/* Error if user tries to divide / (47) or % (37) by 0*/
-	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(operator)(num1, num2));
-
+	printf("%d\n", op_func(num1, num2));
 	return (0);
 }
